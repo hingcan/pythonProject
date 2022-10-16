22767323d1dbf4e59e17e3bfd30477bd21205cfb
Roman Kashlev <hingcan@icloud.com>
2022-10-16 15:34:07 +0600

    create basic pyAutotest create requirements import pytest 7.1.3 create environment "pytest in tests" test is covered division function in utils.py

---
+++ tests/test_division_func.py
@@ -0,0 +1,17 @@
+from utils import division
+import pytest
+
+@pytest.mark.parametrize("a, b, exp_result", [(22, 2, 11),
+                                              (22, 1, 22),
+                                              (6, 6, 1),
+                                              (22, -2, -11),
+                                              (10, 4, 2.5)])
+def test_division_good(a, b, exp_result):
+    assert division(a, b) == exp_result
+
+@pytest.mark.parametrize("exp_exception, divisionable, divider", [(ZeroDivisionError, 10, 0),
+                                                    (TypeError, 20, "0")])
+
+def test_division_zero(exp_exception, divisionable, divider):
+    with pytest.raises(exp_exception):
+        division(divisionable, divider)
