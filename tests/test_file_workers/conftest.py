809f195bc9f87ffc6925858c9164183c4e16b6cd
Roman Kashlev <hingcan@icloud.com>
2022-11-07 00:38:22 +0300

    created new file 'testfile.txt' (replica of 'prodfile.txt') changed path to testfile.txt to absolute path

--- tests/test_file_workers/conftest.py
+++ tests/test_file_workers/conftest.py
@@ -6,7 +6,7 @@ cnt = 0
 @pytest.fixture(autouse=True)
 def testfile_preparation():
     global cnt
-    with open("testfile.txt", "w"):
+    with open("/Users/romankashlev/PycharmProjects/pythonProject/tests/test_file_workers/testfile.txt", "w"):
         pass
     print(cnt)
     cnt += 1
